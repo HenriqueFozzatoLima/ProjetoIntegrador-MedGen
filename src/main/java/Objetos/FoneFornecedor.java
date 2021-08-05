77e8ee9d66163e8e1c8715e539f6141f8bde6c41
kaiop <kaiop@LAPTOP-BB30E84V.Dlink>
2021-08-05 20:57:42 -0300

    Adicionado Classe FoneForncedor

---
+++ src/main/java/Objetos/FoneFornecedor.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author kaiop
+ */
+public class FoneFornecedor {
+    
+    private int codFornecedor;
+    private int telLab;
+
+    /**
+     * @return the codFornecedor
+     */
+    public int getCodFornecedor() {
+        return codFornecedor;
+    }
+
+    /**
+     * @param codFornecedor the codFornecedor to set
+     */
+    public void setCodFornecedor(int codFornecedor) {
+        this.codFornecedor = codFornecedor;
+    }
+
+    /**
+     * @return the telLab
+     */
+    public int getTelLab() {
+        return telLab;
+    }
+
+    /**
+     * @param telLab the telLab to set
+     */
+    public void setTelLab(int telLab) {
+        this.telLab = telLab;
+    }
+    
+    
+    
+}
