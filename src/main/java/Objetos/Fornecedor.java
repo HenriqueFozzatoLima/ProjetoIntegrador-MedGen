d242ad35bb179db93f789115ab99c7a11231bc0c
kaiop <kaiop@LAPTOP-BB30E84V.Dlink>
2021-08-05 20:07:28 -0300

    Adicionado Pacote de Objetos

---
+++ src/main/java/Objetos/Fornecedor.java
@@ -0,0 +1,212 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author kaiop
+ */
+public class Fornecedor {
+    
+   private String nomeFornecedor;
+   private String cnpj;
+   private String inscricaoEstadual;
+   private String estado;
+   private String cidade;
+   private int cep;
+   private String bairro;
+   private String rua;
+   private String email;
+   private String complemento;
+   private int numero;
+   private int codForncedor;
+   private String razaoSocial;
+
+    /**
+     * @return the nomeFornecedor
+     */
+    public String getNomeFornecedor() {
+        return nomeFornecedor;
+    }
+
+    /**
+     * @param nomeFornecedor the nomeFornecedor to set
+     */
+    public void setNomeFornecedor(String nomeFornecedor) {
+        this.nomeFornecedor = nomeFornecedor;
+    }
+
+    /**
+     * @return the cnpj
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * @return the inscricaoEstadual
+     */
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    /**
+     * @param inscricaoEstadual the inscricaoEstadual to set
+     */
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the cep
+     */
+    public int getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return the rua
+     */
+    public String getRua() {
+        return rua;
+    }
+
+    /**
+     * @param rua the rua to set
+     */
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the complemento
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * @param complemento the complemento to set
+     */
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the codForncedor
+     */
+    public int getCodForncedor() {
+        return codForncedor;
+    }
+
+    /**
+     * @param codForncedor the codForncedor to set
+     */
+    public void setCodForncedor(int codForncedor) {
+        this.codForncedor = codForncedor;
+    }
+
+    /**
+     * @return the razaoSocial
+     */
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    /**
+     * @param razaoSocial the razaoSocial to set
+     */
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+    
+    
+    
+}
---
+++ src/main/java/Objetos/Produto.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author kaiop
+ */
+public class Produto {
+    
+    private int codProd;
+    private double valCusto;
+    private double valVenda;
+    private String descProd;
+
+    /**
+     * @return the codProd
+     */
+    public int getCodProd() {
+        return codProd;
+    }
+
+    /**
+     * @param codProd the codProd to set
+     */
+    public void setCodProd(int codProd) {
+        this.codProd = codProd;
+    }
+
+    /**
+     * @return the valCusto
+     */
+    public double getValCusto() {
+        return valCusto;
+    }
+
+    /**
+     * @param valCusto the valCusto to set
+     */
+    public void setValCusto(double valCusto) {
+        this.valCusto = valCusto;
+    }
+
+    /**
+     * @return the valVenda
+     */
+    public double getValVenda() {
+        return valVenda;
+    }
+
+    /**
+     * @param valVenda the valVenda to set
+     */
+    public void setValVenda(double valVenda) {
+        this.valVenda = valVenda;
+    }
+
+    /**
+     * @return the descProd
+     */
+    public String getDescProd() {
+        return descProd;
+    }
+
+    /**
+     * @param descProd the descProd to set
+     */
+    public void setDescProd(String descProd) {
+        this.descProd = descProd;
+    }
+    
+    
+    
+}
