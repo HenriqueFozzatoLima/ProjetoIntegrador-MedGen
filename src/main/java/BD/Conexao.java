0ce487a5ff90f614a2b3263b04816dd9c0f0fc41
user <user@10.0.0.8>
2021-08-05 21:26:34 -0300

    Tentando ver o problema com a conexão do banco

--- src/main/java/BD/Conexao.java
+++ src/main/java/BD/Conexao.java
@@ -19,10 +19,9 @@ import javax.swing.JOptionPane;
 public class Conexao {
   
     private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/ProjetoIntegradorMedGen";
+    private static final String URL = "jdbc:mysql://localhost:3306/BD_MedGen";
     private static final String USER = "root";
     private static final String PASS = "123456";
-    
 
         public static Connection getConnection(){
             try {
