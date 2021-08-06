c2eeb7957c90b13d5543d8ff86427092fb14e5c2
user <user@10.0.0.8>
2021-08-05 21:27:13 -0300

    Criação do prodtuo Dao e iniciação do FornecedorDao

---
+++ src/main/java/DAO/ProdutoDAO.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import BD.Conexao;
+import Objetos.Produto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class ProdutoDAO {
+
+    /*
+    public List<Produto> read() {
+        Connection con = Conexao.getConection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Produto> produtos = new ArrayList<>();
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM tbl_produto");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Produto p = new Produto();
+                p.setId(rs.getInt("id"));
+                p.setDescricao(rs.getString("descricao"));
+                p.setValor(rs.getDouble("valor"));
+                p.setQuantidade(rs.getInt("quantidade"));
+                produtos.add(p);
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Falha ao obter dados. " + "\n" + e);
+        } finally {
+            Conexao.closeConnection(con, stmt, rs);
+        }
+        return produtos;
+    }
+     */
+
+    public List<Produto> read() {
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Produto> produtos = new ArrayList<>();
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM tbl_produto");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Produto p = new Produto();
+                p.setCodProd(rs.getInt("CodProd"));
+                p.setDescProd(rs.getString("DescrProd"));
+                p.setValCusto(rs.getDouble("ValCusto"));
+                p.setValVenda(rs.getDouble("ValVenda"));
+                p.setQtdeProd(rs.getInt("Qtde"));
+                produtos.add(p);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Falha ao obter dados: " + e);
+        } finally {
+            Conexao.closeConnection(con, stmt, rs);
+        }
+        return produtos;
+    }
+
+    public void create(Produto p) {
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("INSERT INTO tbl_produto (DESC_PROD, VAL_CUSTO, VAL_VENDA, QTDE_PROD) VALUES (?, ?, ?, ?)");
+            stmt.setString(1, p.getDescProd());
+            stmt.setDouble(2, p.getValCusto());
+            stmt.setDouble(3, p.getValVenda());
+            stmt.setInt(4, p.getQtdeProd());
+
+            stmt.execute();
+            
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Falha ao cadastrar. " + "\n" + e);
+        } finally {
+            Conexao.closeConnection(con, stmt);
+        }
+    }
+
+      public void update(Produto p) {
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("UPDATE tbl_produto SET DESC_PROD = ?, VAL_CUSTO = ?, VAL_VENDA = ?, QTDE_PROD = ? WHERE COD_PROD = ?");
+            stmt.setString(1, p.getDescProd());
+            stmt.setDouble(2, p.getValCusto());
+            stmt.setDouble(3, p.getValCusto());
+            stmt.setInt(4, p.getQtdeProd());
+            stmt.setInt(5, p.getCodProd());
+
+            stmt.execute();
+            
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Falha ao atualizar. " + "\n" + e);
+        } finally {
+            Conexao.closeConnection(con, stmt);
+        }
+    }
+    
+      public void delete(Produto p) {
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+        
+        try {
+            stmt = con.prepareStatement("DELETE FROM tbl_produto WHERE COD_PROD = ?");
+            stmt.setInt(1, p.getCodProd());
+            stmt.execute();
+                   
+            
+            
+        } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Falha ao remover. " + "\n" + e);
+        } finally {
+            Conexao.closeConnection(con, stmt);
+        }
+
+    }
+    
+    
+}
